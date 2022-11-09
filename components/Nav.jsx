b80b0066bd32cc2565e0d3d55c38b7ba09d2c9b0
DevHumbleChris <dev.humblechris254@gmail.com>
2022-11-09 17:32:00 +0300

    Reduced  made to cols in small device

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ export default function Nav() {
   const router = useRouter();
   return (
     <nav>
-      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 overflow-x-scroll scrollbar-hide">
+      <div className="flex px-4 sm:px-5 text-2xl whitespace-nowrap space-x-10 overflow-x-scroll scrollbar-hide">
         {Object.entries(requests).map(([key, { title, url }]) => {
           return (
             <h2
