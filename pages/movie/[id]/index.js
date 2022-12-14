4f45361bc106256d888de3525a63964cc6afe392
DevHumbleChris <dev.humblechris254@gmail.com>
2022-12-14 16:42:51 +0300

    Added casts

--- pages/movie/[id]/index.js
+++ pages/movie/[id]/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import React from "react";
-import axios from 'axios'
+import axios from "axios";
 
 export default function index({ result, castResult }) {
   console.log(castResult);
@@ -64,35 +64,40 @@ export default function index({ result, castResult }) {
             </div>
             <div className="my-2">
               <h3 className="text-xl my-3">Top Billed Cast</h3>
-              <div>
+              <div className="flex gap-3 pb-2 overflow-x-auto snap-x">
                 {castResult.cast.map((actor) => {
                   console.log(actor);
+                  return (
+                    <div
+                      key={actor.id}
+                      className="relative snap-start scroll-ml-6 shrink-0 first:pl-6 last:pr-6"
+                    >
+                      <div className="relative flex flex-col overflow-hidden transition-all duration-200 transform bg-white border border-gray-100 shadow w-60 md:w-80 group rounded-xl hover:shadow-lg hover:-translate-y-1">
+                        <a
+                          href="#"
+                          title=""
+                          className="flex shrink-0 aspect-w-4 aspect-h-3"
+                        >
+                          <Image
+                            className="object-cover w-full h-full transition-all duration-200 transform group-hover:scale-110"
+                            width="400"
+                            height="400"
+                            src="https://www.themoviedb.org/t/p/w138_and_h175_face/hCi43gRO7zfv3Mu8X1bpqtloyHT.jpg"
+                            alt="thumbnail-3"
+                          />
+                        </a>
+                        <div className="flex-1 px-4 py-5 sm:p-6">
+                          <p className="text-sm font-bold text-gray-900">
+                            How to write content about your photographs
+                          </p>
+                          <p className="mt-3 text-sm font-normal leading-6 text-gray-500 line-clamp-3">
+                            Lorem ipsum dolor sit amet
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  );
                 })}
-              </div>
-              <div className="relative snap-start scroll-ml-6 shrink-0 first:pl-6 last:pr-6">
-                <div className="relative flex flex-col overflow-hidden transition-all duration-200 transform bg-white border border-gray-100 shadow w-60 md:w-80 group rounded-xl hover:shadow-lg hover:-translate-y-1">
-                  <a
-                    href="#"
-                    title=""
-                    className="flex shrink-0 aspect-w-4 aspect-h-3"
-                  >
-                    <Image
-                      className="object-cover w-full h-full transition-all duration-200 transform group-hover:scale-110"
-                      width="400"
-                      height="400"
-                      src="https://www.themoviedb.org/t/p/w138_and_h175_face/hCi43gRO7zfv3Mu8X1bpqtloyHT.jpg"
-                      alt="thumbnail-3"
-                    />
-                  </a>
-                  <div className="flex-1 px-4 py-5 sm:p-6">
-                    <p className="text-sm font-bold text-gray-900">
-                      How to write content about your photographs
-                    </p>
-                    <p className="mt-3 text-sm font-normal leading-6 text-gray-500 line-clamp-3">
-                      Lorem ipsum dolor sit amet
-                    </p>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
@@ -105,12 +110,16 @@ export default function index({ result, castResult }) {
 
 export async function getServerSideProps({ query }) {
   const { id } = query;
-  const result = await axios.get(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}`
-  ).then((res) => res.data);
-  const castResult = await axios.get(
-    `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${process.env.API_KEY}=en-US`
-  ).then((res) => res.data);
+  const result = await axios
+    .get(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}`
+    )
+    .then((res) => res.data);
+  const castResult = await axios
+    .get(
+      `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${process.env.API_KEY}=en-US`
+    )
+    .then((res) => res.data);
   return {
     props: {
       result,
